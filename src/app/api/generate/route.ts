e5cd36c8fd4ed66fff3cc9194a78d4cd39a2386b
Kei-K23 <keiksl2301@gmail.com>
2024-09-28 09:39:27 +0630

    Add route handler to generate short url hash value

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,5 +1,8 @@
+import { db } from "@/db/drizzle";
+import { urlMappings } from "@/db/schema";
 import { generateHash } from "@/lib/hash";
 import { NextRequest, NextResponse } from "next/server";
+import { createId } from "@paralleldrive/cuid2";
 
 export const revalidate = 0;
 
@@ -7,8 +10,26 @@ export async function POST(request: NextRequest) {
   const resBody = await request.json();
   const longURL = resBody.longURL;
 
-  // Generated hash value is shortURL
-  const shortURL = await generateHash(longURL);
+  try {
+    // Generated hash value is shortURL
+    const shortURLHashValue = await generateHash(longURL);
 
-  return NextResponse.json({ shortURL }, { status: 200 });
+    const [urlMapping] = await db
+      .insert(urlMappings)
+      .values({ id: createId(), longUrl: longURL, shortUrl: shortURLHashValue })
+      .returning({
+        shortURL: urlMappings.shortUrl,
+      });
+
+    return NextResponse.json(
+      { shortURL: urlMapping.shortURL },
+      { status: 200 }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,7 @@ import { createInsertSchema } from "drizzle-zod";
 export const urlMappings = pgTable("url_mappings", {
   id: text("id").primaryKey(),
   longUrl: text("long_url").notNull(),
-  shortUrl: text("short_url"),
+  shortUrl: text("short_url").notNull().unique(),
   createdAt: timestamp("created_at").defaultNow(),
 });
 
