7cc4e587851ac5449e62e39eaefa87d64e6c8fe3
Kei-K23 <keiksl2301@gmail.com>
2024-09-28 11:02:56 +0630

    Add utility function to get and remove short url data item from localStorage

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -14,3 +14,24 @@ export function setStorageItems(key: string, value: URLMapping) {
   // Store the updated array back to localStorage
   localStorage.setItem(key, JSON.stringify(existingItems));
 }
+
+export function getStorageItems(key: string) {
+  return JSON.parse(localStorage.getItem(key) || "[]") as URLMapping[];
+}
+
+export function removeAllStorageItems(key: string) {
+  localStorage.removeItem(key);
+}
+
+export function removeStorageItemsById(key: string, hashKey: string) {
+  // Retrieve existing array from localStorage or initialize an empty array
+  let existingItems = JSON.parse(
+    localStorage.getItem(key) || "[]"
+  ) as URLMapping[];
+
+  // Remove item
+  existingItems = existingItems.filter((item) => item.shortUrl !== hashKey);
+
+  // Store the updated array back to localStorage
+  localStorage.setItem(key, JSON.stringify(existingItems));
+}
