c45bfbe942e4955c779bd24bc0760f4ad59ba384
Kei-K23 <keiksl2301@gmail.com>
2024-09-28 08:45:06 +0630

    Setup client request handler to generate short url

--- src/components/create-shorten-url.tsx
+++ src/components/create-shorten-url.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import {
@@ -8,16 +8,60 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useToast } from "@/hooks/use-toast";
 
 type CreateShortenUrlProps = {
   longURL: string;
   setLongURL: React.Dispatch<React.SetStateAction<string>>;
+  setShortURL: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export default function CreateShortenUrl({
   longURL,
   setLongURL,
+  setShortURL,
 }: CreateShortenUrlProps) {
+  // Define basic loading and error state (I use manual way because this project is quite small to use other data fetching and state management tools)
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (longURL === "") return;
+
+    setIsLoading(true);
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/generate`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        longURL,
+      }),
+      cache: "no-store",
+    });
+    const json = await res.json();
+
+    if (res.ok && json.shortURL) {
+      setShortURL("json.shortURL");
+      setLongURL("");
+      setIsLoading(false);
+      return;
+    } else {
+      // Response have error
+      setIsLoading(false);
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      });
+      return;
+    }
+  };
+
   return (
     <Card className="max-w-2xl mt-10 md:mt-12 mx-auto">
       <CardHeader>
@@ -26,14 +70,17 @@ export default function CreateShortenUrl({
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <form className="flex h-10 md:h-12">
+        <form onSubmit={handleSubmit} className="flex h-10 md:h-12">
           <Input
+            disabled={isLoading}
             value={longURL}
             onChange={(e) => setLongURL(e.target.value)}
             className="h-full text-sm md:text-[16px]"
             placeholder="Paste your long URL here..."
           />
           <Button
+            disabled={isLoading}
+            type="submit"
             variant={"secondary"}
             className="h-full text-sm md:text-[16px]"
           >
--- src/components/home-screen.tsx
+++ src/components/home-screen.tsx
@@ -6,6 +6,7 @@ import CopyShortenedUrl from "./copy-shortened-url";
 
 export default function HomeScreen() {
   const [longURL, setLongURL] = useState<string>("");
+  const [shortURL, setShortURL] = useState<string>("");
 
   return (
     <main className="p-4">
@@ -15,13 +16,21 @@ export default function HomeScreen() {
       <h2 className="mt-3 text-center text-2xl md:text-4xl text-neutral-300">
         Shorten Your URLs with Ease
       </h2>
-      <CreateShortenUrl longURL={longURL} setLongURL={setLongURL} />
-      <CopyShortenedUrl
-        longURL={
-          "https://chat.openai.com/c/c592b21c-aeff-4b8b-b6d0-d906b19cdd9a"
-        }
-        shortURL="https://chat.openai.com"
-      />
+      {shortURL === "" && (
+        <CreateShortenUrl
+          longURL={longURL}
+          setLongURL={setLongURL}
+          setShortURL={setShortURL}
+        />
+      )}
+      {shortURL !== "" && (
+        <CopyShortenedUrl
+          longURL={
+            "https://chat.openai.com/c/c592b21c-aeff-4b8b-b6d0-d906b19cdd9a"
+          }
+          shortURL="https://chat.openai.com"
+        />
+      )}
     </main>
   );
 }
